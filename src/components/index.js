b41ff6f13d9a3255848ba4bb9fad22531082f2b1
Luca Iaconelli <lucabelli@msn.com>
2020-04-05 04:54:18 +0200

    feat: article

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as DayNightToggle } from './DayNightToggle';
 export { default as Countdown } from './Countdown';
 export { default as CountryItem } from './CountryItem';
+export { default as Notizia } from './Notizia';
